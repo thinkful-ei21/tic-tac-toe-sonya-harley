ac453d4f0c7448085c7cd5a6b85eb8d0ed99b2e5
Sonya Slegers <sonjay08@gmail.com>
2018-05-14 16:25:00 -0500

    added handPlay and handleNewGameButton method

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
+'use strict';
+
 const state = {
+  playCharacter: 'x',
+  cellValue: ['','','','','','','','','']
 };
+
+function handleNewGameButton() {
+  $('#new-game').click(function() {
+    location.reload();
+  });
+}
+
+function handlePlay() {
+  $('.board').on('click', '.cell', function(event) {
+    console.log(this);
+    const cellId = parseInt($(this).attr('id'), 10);
+    state.cellValue[cellId] = state.playCharacter;
+    console.log(cellId);
+
+  } );
+}
+
+function render(id) {
+  
+}
+function bindEventListeners() {
+
+}
+
+function handleWin() {}
+
+function checkForWin() {}
 
 // State modification functions
 
 // Render functions
 
 // Event Listeners
+
+
+function main () {
+  render(),
+  bindeEventListeners()
+}
+
+$(main());
+
+
+
+
